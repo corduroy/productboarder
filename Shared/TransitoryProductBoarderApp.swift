cb9ad215aa8014bf52e0fdc9e9bf3e4a55978d2b
Joshua McKinnon <josh@corduroy.biz>
2021-12-21 21:14:51 +1100

    Initial Commit

---
+++ Shared/TransitoryProductBoarderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitoryProductBoarderApp.swift
+//  Shared
+//
+//  Created by Joshua McKinnon on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TransitoryProductBoarderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
